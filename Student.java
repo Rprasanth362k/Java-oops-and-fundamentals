523b2f1775fd4764e3e0dcedb930083c8bbd828e
prasanth362k <prasanth362k@gmail.com>
2024-10-16 18:55:08 +0530

    commit by prasanth

---
+++ Student.java
@@ -0,0 +1,16 @@
+class Student
+{
+int age;
+String name;
+String college;
+public static void main(String[] args)
+
+
+{
+Student  student = new Student();
+ student.age=24;
+ student.name="Prasanth";
+ student.college="jaya college";
+System.out.println(student.name+" "+student.age+" "+student.college);
+}
+}
