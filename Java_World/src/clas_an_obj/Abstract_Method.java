09bccb73fefa8b02c60e67e0634656a3f333e09b
prasanth362k <prasanth362k@gmail.com>
2024-10-18 16:17:54 +0530

    commited by prasanth

---
+++ Java_World/src/clas_an_obj/Abstract_Method.java
@@ -0,0 +1,17 @@
+package clas_an_obj;
+
+public class Abstract_Method {
+
+	public static void main(String[] args) {
+		student_info();
+		
+		
+	}
+	static void student_info() {
+		String name="Prasanth",college_Name="jaya Arts & Science college.";
+		int age=24,roll_no=101;
+
+		System.out.println("Name: "+ name+", Roll_no: "+roll_no+", Age: "+age+", College_Name: "+college_Name);
+	}
+
+}
