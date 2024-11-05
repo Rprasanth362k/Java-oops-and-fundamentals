fd1ab8b8920e1ab05eef56d02c893f18e375866e
prasanth362k <prasanth362k@gmail.com>
2024-11-05 21:02:18 +0530

    commit

---
+++ Java_World/src/control_Flow/Dowhile_Example1.java
@@ -0,0 +1,24 @@
+package control_Flow;
+
+public class Dowhile_Example1 {
+
+	public static void main(String[] args) {
+		int add=10;
+		do
+		{
+			System.out.println("do Block "+add);
+			add++;	
+		
+		}
+		
+		while(add<=15);		{
+			System.out.println("while Block "+add);
+			
+		}
+		
+
+
+	}
+
+}
+
